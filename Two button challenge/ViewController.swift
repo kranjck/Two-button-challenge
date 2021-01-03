4d01e9b920e3f82fefeebc40f2c5901de9ba0083
Kristen Kranjc <kranjck@bc.edu>
2021-01-02 19:07:29 -0800

    Ch 2.4—Buttons change colors

--- Two button challenge/ViewController.swift
+++ Two button challenge/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = UIColor.blue
+    }
+    @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+    }
+    
 
 }
 
